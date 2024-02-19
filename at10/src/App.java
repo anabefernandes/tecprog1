4740f98ffc8726a55f7a6138a34fe4237f3d6f68
Ana Beatriz <abeatriz.fernandes@outlook.com>
2024-02-18 21:42:25 -0300

    atividades 8 e 10

--- at10/src/App.java
+++ at10/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Senha Secreta: Defina uma senha secreta (por exemplo: "Java123"). 
+        //Use um loop while para pedir ao usuário que insira a senha. 
+        //Se a senha estiver incorreta, continue pedindo a senha e informe ao usuário que a tentativa foi inválida. 
+        //Se ele acertar, saia do loop e imprima uma mensagem de sucesso
+
+        Scanner scanner = new Scanner(System.in);
+        String senhaCorreta = "java123";
+        String entrada;
+        do{
+            System.out.println("Insira a senha correta: ");
+            entrada = scanner.nextLine();
+            }while (!entrada.equals(senhaCorreta));
+        System.out.println("Parabens! Senha correta.");
+        scanner.close();
     }
 }
--- at8/src/App.java
+++ at8/src/App.java
@@ -6,10 +6,17 @@ public class App {
         //Calcule e imprima a soma dos primeiros n números ímpares. 
         //Por exemplo, se o usuário inserir 4, o programa deve calcular a soma de 1 + 3 + 5 + 7 = 16.
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite um número: ");
+        System.out.println("Digite um número inteiro: ");
         int n = scanner.nextInt();
-        int [] numero = new int[n];
-        for
+        int soma = 0;
+        int numImpar = 1;
+        for(int i=0; i<n; i++){
+            soma = soma+numImpar;
+            numImpar = numImpar+2;
+        }
+        System.out.println("A soma dos "+ n +" primeiros números impares é: "+ soma);
 
+        scanner.close();
     }
+    
 }
